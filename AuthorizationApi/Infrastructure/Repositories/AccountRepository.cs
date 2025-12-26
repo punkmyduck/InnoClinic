3ab2a54f804daf2f89becaaacc539a22dfe1ea5a
punkmyduck <punkmyduck@mail.ru>
2025-12-26 18:09:33 +0300

    Created Account repository

---
+++ AuthorizationApi/Infrastructure/Repositories/AccountRepository.cs
@@ -0,0 +1,25 @@
+﻿using AuthorizationApi.Domain.Models;
+using AuthorizationApi.Domain.Repositories;
+using AuthorizationApi.Domain.ValueObjects;
+
+namespace AuthorizationApi.Infrastructure.Repositories
+{
+    //TODO: implement repository methods
+    public class AccountRepository : IAccountRepository
+    {
+        private AuthDbContext _context;
+        public AccountRepository(AuthDbContext context)
+        {
+            _context = context;
+        }
+        public Task AddAsync(Account account)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Account?> GetByEmailAsync(Email email)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
