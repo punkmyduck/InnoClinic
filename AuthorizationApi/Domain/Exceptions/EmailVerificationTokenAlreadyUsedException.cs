3ae93035a54daf273aa04e9ed311cf8f4f0df6fe
punkmyduck <punkmyduck@mail.ru>
2025-12-26 18:36:02 +0300

    EmailVerificationToken model added

---
+++ AuthorizationApi/Domain/Exceptions/EmailVerificationTokenAlreadyUsedException.cs
@@ -0,0 +1,18 @@
+﻿namespace AuthorizationApi.Domain.Exceptions
+{
+    public class EmailVerificationTokenAlreadyUsedException : DomainException
+    {
+        public EmailVerificationTokenAlreadyUsedException()
+        {
+
+        }
+
+        public EmailVerificationTokenAlreadyUsedException(string message) : base(message)
+        {
+        }
+
+        public EmailVerificationTokenAlreadyUsedException(string message, Exception inner) : base(message, inner)
+        {
+        }
+    }
+}
