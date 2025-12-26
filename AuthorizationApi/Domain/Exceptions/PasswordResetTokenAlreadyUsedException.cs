e712daa54f3ef2785860f6c72957b25a2270dfcf
punkmyduck <punkmyduck@mail.ru>
2025-12-26 18:43:56 +0300

    PasswordResetToken model added

---
+++ AuthorizationApi/Domain/Exceptions/PasswordResetTokenAlreadyUsedException.cs
@@ -0,0 +1,11 @@
+﻿namespace AuthorizationApi.Domain.Exceptions
+{
+    public class PasswordResetTokenAlreadyUsedException : DomainException
+    {
+        public PasswordResetTokenAlreadyUsedException() { }
+
+        public PasswordResetTokenAlreadyUsedException(string message) : base(message) { }
+
+        public PasswordResetTokenAlreadyUsedException(string message, Exception inner) : base(message, inner) { }
+    }
+}
---
+++ AuthorizationApi/Domain/ValueObjects/PasswordResetTokenId.cs
@@ -0,0 +1,4 @@
+﻿namespace AuthorizationApi.Domain.ValueObjects
+{
+    public sealed record PasswordResetTokenId(Guid Value);
+}
