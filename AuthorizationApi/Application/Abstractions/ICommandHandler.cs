6b765642d8c9e76637b414663d8485e9c96c53c8
punkmyduck <punkmyduck@mail.ru>
2025-12-27 17:53:17 +0300

    Created command-handler pattern

---
+++ AuthorizationApi/Application/Abstractions/ICommandHandler.cs
@@ -0,0 +1,9 @@
+﻿namespace AuthorizationApi.Application.Abstractions
+{
+    public interface ICommandHandler<C, R> 
+        where C : ICommand 
+        where R : ICommandResult
+    {
+        public Task<R> HandleAsync(C command, CancellationToken cancellationToken);
+    }
+}
