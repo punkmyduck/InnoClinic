fbd63d0071e40554d53f13b271b870ebbf43e080
punkmyduck <punkmyduck@mail.ru>
2025-12-27 18:29:15 +0300

    Services registered in DI

---
+++ AuthorizationApi/Application/Extensions.cs
@@ -0,0 +1,14 @@
+﻿namespace AuthorizationApi.Application
+{
+    public static class Extensions
+    {
+        public static void AddApplicationLayer(this IServiceCollection services)
+        {
+            services.AddScoped<
+                Abstractions.ICommandHandler<
+                    Commands.RegisterCommand,
+                    Results.RegisterCommandResult>,
+                Handlers.RegisterUserHandler>();
+        }
+    }
+}
