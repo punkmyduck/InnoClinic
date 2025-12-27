b38b73b5cbe0569da91dd756a05729d08aaf5971
punkmyduck <punkmyduck@mail.ru>
2025-12-27 18:01:24 +0300

    Created user's registration handler with contracts

---
+++ AuthorizationApi/Application/Commands/RegisterCommand.cs
@@ -0,0 +1,17 @@
+﻿using AuthorizationApi.Application.Abstractions;
+
+namespace AuthorizationApi.Application.Commands
+{
+    public class RegisterCommand : ICommand
+    {
+        public string Email { get; }
+        public string Password { get; }
+        public string? PhoneNumber { get; }
+        public RegisterCommand(string email, string password, string? phoneNumber)
+        {
+            Email = email;
+            Password = password;
+            PhoneNumber = phoneNumber;
+        }
+    }
+}
---
+++ AuthorizationApi/Application/Handlers/RegisterUserHandler.cs
@@ -0,0 +1,29 @@
+﻿using AuthorizationApi.Application.Abstractions;
+using AuthorizationApi.Application.Commands;
+using AuthorizationApi.Application.Results;
+
+namespace AuthorizationApi.Application.Handlers
+{
+    public class RegisterUserHandler : ICommandHandler<RegisterCommand, RegisterCommandResult>
+    {
+        public RegisterUserHandler()
+        {
+            
+        }
+        public Task<RegisterCommandResult> HandleAsync(RegisterCommand command, CancellationToken cancellationToken)
+        {
+            //TODO: реализовать регистрацию пользователя
+            //проверить существование почты
+            //проверить надежность пароля
+            //создать хеш пароля
+            //создать гуид пользователя
+            //создать сущность пользователя
+            //создать верификационный токен почты
+            //сохранить пользователя в бд
+            //сохранить токен в бд
+            //закоммитить работу
+            //вернуть результат с гуидом пользователя
+            throw new NotImplementedException();
+        }
+    }
+}
