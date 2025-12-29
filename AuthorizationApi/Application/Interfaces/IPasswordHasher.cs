736ac56dd1de802ebf84133668cbc6f347313cbe
punkmyduck <punkmyduck@mail.ru>
2025-12-29 18:33:04 +0300

    Added PasswordHasher class

---
+++ AuthorizationApi/Application/Interfaces/IPasswordHasher.cs
@@ -0,0 +1,8 @@
+﻿namespace AuthorizationApi.Application.Interfaces
+{
+    public interface IPasswordHasher
+    {
+        string GenerateHash(string password);
+        bool VerifyPassword(string password, string hash);
+    }
+}
