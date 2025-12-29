3e89b3391b336c9e335c56ae235af6604bd94d9a
punkmyduck <punkmyduck@mail.ru>
2025-12-29 21:08:59 +0300

    JwtClaimsFactory added

---
+++ AuthorizationApi/Infrastructure/Security/JwtClaimsFactory.cs
@@ -0,0 +1,22 @@
+﻿using AuthorizationApi.Domain.Models;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace AuthorizationApi.Infrastructure.Security
+{
+    public static class JwtClaimsFactory
+    {
+        public static IReadOnlyCollection<Claim> Create(Account account, string issuer)
+        {
+            var now = DateTimeOffset.UtcNow;
+
+            return new List<Claim>
+            {
+                new(JwtRegisteredClaimNames.Sub, account.Id.Value.ToString()),
+                new(JwtRegisteredClaimNames.Email, account.Email.Value),
+                new(JwtRegisteredClaimNames.Iat, now.ToUnixTimeSeconds().ToString(), ClaimValueTypes.Integer64),
+                new(JwtRegisteredClaimNames.Iss, issuer)
+            };
+        }
+    }
+}
