07518e7ff2fc32022205c3f3e59375f11787569d
punkmyduck <punkmyduck@mail.ru>
2025-12-29 20:02:39 +0300

    Added repository for EmailVerificationToken

---
+++ AuthorizationApi/Infrastructure/Repositories/EmailVerificationTokenRepository.cs
@@ -0,0 +1,21 @@
+﻿using AuthorizationApi.Application.Interfaces;
+using AuthorizationApi.Domain.Models;
+using AuthorizationApi.Infrastructure.Mappers;
+
+namespace AuthorizationApi.Infrastructure.Repositories
+{
+    public class EmailVerificationTokenRepository : IEmailVerificationTokenRepository
+    {
+        private readonly AuthDbContext _context;
+        public EmailVerificationTokenRepository(AuthDbContext context)
+        {
+            _context = context;
+        }
+        public async Task AddAsync(EmailVerificationToken token, CancellationToken cancellationToken)
+        {
+            var entity = EmailVerificationTokenMapper.ToEntity(token);
+            _context.EmailVerificationTokens.Add(entity);
+            await Task.CompletedTask;
+        }
+    }
+}
