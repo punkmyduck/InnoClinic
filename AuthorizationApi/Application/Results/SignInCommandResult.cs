5ea36dfe2ecec6c289d5ab88c16877e9cd795138
punkmyduck <punkmyduck@mail.ru>
2025-12-29 21:08:09 +0300

    Added some fields

--- AuthorizationApi/Application/Results/SignInCommandResult.cs
+++ AuthorizationApi/Application/Results/SignInCommandResult.cs
@@ -5,9 +5,15 @@ namespace AuthorizationApi.Application.Results
     public class SignInCommandResult : ICommandResult
     {
         public bool IsSuccessful { get; }
-        public SignInCommandResult(bool isSuccessful)
+        public string? JwtToken { get; }
+        public DateTime? ExpiresAt { get; }
+        public string? RefreshToken { get; }
+        public SignInCommandResult(bool isSuccessful, string? jwtToken = null, DateTime? expiresAt = null, string? refreshToken = null)
         {
             IsSuccessful = isSuccessful;
+            JwtToken = jwtToken;
+            ExpiresAt = expiresAt;
+            RefreshToken = refreshToken;
         }
     }
 }
