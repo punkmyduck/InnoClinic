084b37c9d8b3ecc5075f8653c78921698c7ce307
punkmyduck <punkmyduck@mail.ru>
2025-12-30 16:22:48 +0300

    RefreshTokenRepository implementation

---
+++ AuthorizationApi/Infrastructure/Repositories/RefreshTokenRepository.cs
@@ -0,0 +1,32 @@
+﻿using AuthorizationApi.Application.Interfaces;
+using AuthorizationApi.Domain.Models;
+using AuthorizationApi.Domain.ValueObjects;
+using AuthorizationApi.Infrastructure.Mappers;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthorizationApi.Infrastructure.Repositories
+{
+    public class RefreshTokenRepository : IRefreshTokenRepository
+    {
+        private readonly AuthDbContext _context;
+        public RefreshTokenRepository(AuthDbContext context)
+        {
+            _context = context;
+        }
+        public async Task AddAsync(RefreshToken refreshToken, CancellationToken cancellationToken)
+        {
+            var entity = RefreshTokenMapper.ToEntity(refreshToken);
+            _context.RefreshTokens.Add(entity);
+            await Task.CompletedTask;
+        }
+
+        public async Task<RefreshToken?> GetTokenByUserId(AccountId accoundId)
+        {
+            var entity = await _context.RefreshTokens
+                .AsNoTracking()
+                .FirstOrDefaultAsync(r => r.AccountId == accoundId.Value);
+            if (entity is null) return null;
+            return RefreshTokenMapper.ToDomain(entity);
+        }
+    }
+}
