8cf2248c4a9d58ac181bd3117e2868b11487e7a0
punkmyduck <punkmyduck@mail.ru>
2025-12-30 19:37:09 +0300

    UoW now has functionality for managing transactions

--- AuthorizationApi/Infrastructure/UnitOfWork/EfUnitOfWork.cs
+++ AuthorizationApi/Infrastructure/UnitOfWork/EfUnitOfWork.cs
@@ -1,17 +1,42 @@
 ﻿using AuthorizationApi.Application.Interfaces;
+using Microsoft.EntityFrameworkCore.Storage;
 
 namespace AuthorizationApi.Infrastructure.UnitOfWork
 {
     public class EfUnitOfWork : IUnitOfWork
     {
         private readonly AuthDbContext _context;
+        private IDbContextTransaction? _transaction;
         public EfUnitOfWork(AuthDbContext context)
         {
             _context = context;
         }
+
+        public async Task BeginAsync(CancellationToken cancellationToken)
+        {
+            _transaction = await _context.Database.BeginTransactionAsync(cancellationToken);
+        }
+
         public async Task CommitAsync(CancellationToken cancellationToken)
         {
             await _context.SaveChangesAsync(cancellationToken);
+            
+            if (_transaction != null)
+            {
+                await _transaction.CommitAsync(cancellationToken);
+                await _transaction.DisposeAsync();
+                _transaction = null;
+            }
+        }
+
+        public async Task RollbackAsync(CancellationToken cancellationToken)
+        {
+            if (_transaction != null)
+            {
+                await _transaction.RollbackAsync(cancellationToken);
+                await _transaction.DisposeAsync();
+                _transaction = null;
+            }
         }
     }
 }
