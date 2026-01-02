4d6f97eea04813b2f218060086570909e8b6ea5b
punkmyduck <punkmyduck@mail.ru>
2026-01-02 08:00:07 +0300

    Added new command and commandresult for refresh handler

---
+++ AuthorizationApi/Application/Results/RefreshCommandResult.cs
@@ -0,0 +1,20 @@
+﻿using AuthorizationApi.Application.Abstractions;
+
+namespace AuthorizationApi.Application.Results
+{
+    public class RefreshCommandResult : ICommandResult
+    {
+        public string? JwtToken { get; }
+        public DateTime? ExpiresAt { get; }
+        public string? RefreshToken { get; }
+        public RefreshCommandResult(
+            string? jwtToken, 
+            DateTime? expiresAt, 
+            string? refreshToken)
+        {
+            JwtToken = jwtToken;
+            ExpiresAt = expiresAt;
+            RefreshToken = refreshToken;
+        }
+    }
+}
