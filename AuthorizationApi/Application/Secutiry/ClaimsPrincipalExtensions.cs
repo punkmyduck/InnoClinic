53e170ff12e2e6bcf962c37c8b6e8141c01c5fb5
punkmyduck <punkmyduck@mail.ru>
2026-01-02 08:00:54 +0300

    Added method for getting user's AccountId from jwt claim

---
+++ AuthorizationApi/Application/Secutiry/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,17 @@
+﻿using AuthorizationApi.Domain.ValueObjects;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace AuthorizationApi.Application.Secutiry
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static AccountId GetAccountId(this ClaimsPrincipal user)
+        {
+            var value = user.FindFirst(JwtRegisteredClaimNames.Sub)?.Value
+                ?? throw new UnauthorizedAccessException("Missing sub claim.");
+
+            return new AccountId(Guid.Parse(value));
+        }
+    }
+}
